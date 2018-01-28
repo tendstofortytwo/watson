f44806d33387809bcb92f57c464f83c982ea65f4
Naman Sood <mail@nsood.in>
2018-01-28 15:21:31 +0000

    Update handler.js API key - 28/01

--- routes/handler.js
+++ routes/handler.js
@@ -128,7 +128,7 @@ router.get('/suggest', function(req, res, next) {
 			hostname: 'api.cognitive.microsoft.com',
 			path: '/bing/v7.0/Suggestions?mkt=en-IN&q=' + encodeURIComponent(req.query.term),
 			headers: {
-				'Ocp-Apim-Subscription-Key': '090d03f5dbf54bc5a91e659b644a5678'
+				'Ocp-Apim-Subscription-Key': '1a051aaffec8403c81462b9efee1bdf9'
 			}
 		}, function(apiResponse) {
 			var body = '';
